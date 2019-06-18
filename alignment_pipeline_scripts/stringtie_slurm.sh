70ead40a3df3c22507c12bd2711608c07705b716
Adam Faranda <abf@udel.edu>
2019-06-18 19:47:33 -0400

    Created wrapper and calling scripts for stringtie transcriptome assembly

--- alignment_pipeline_scripts/stringtie_slurm.sh
+++ alignment_pipeline_scripts/stringtie_slurm.sh
@@ -9,9 +9,7 @@ export GENOME_GTF=/home/abf/MouseEnsembl96/Mus_musculus.GRCm38.96.chr.gtf
 S=$(echo $1 | sed 's/_sorted_alignment\.bam$//')
 S=$(echo $S | awk '{ n=split($NF, a, "/"); print a[n]}')
 
-echo $OUTDIR
-echo $OUTDIR/$S
-stringtie $1 -p4 --fr -e -B\
+echo stringtie $1 -p4 --fr -e -B\
 	  -G $GENOME_GTF\
 	  -o $OUTDIR"/"$S"/"$S.gtf\
 	  -A $OUTDIR"/"$S"/"$S.txt
